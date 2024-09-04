bba8034b4e0aeb0c6fbb6bd90f28937b4fa17314
Shota Muto <129914139+MutoShota@users.noreply.github.com>
2024-09-04 17:51:58 +0900

    メトリクス取得処理を実装

--- src/app.py
+++ src/app.py
@@ -3,8 +3,10 @@ from flask import Flask, jsonify, request
 from flask.logging import create_logger
 from datetime import datetime, timezone
 from pathlib import Path
-
+from math import floor
 from promclient import PrometheusClient
+from collections import defaultdict
+import re
 
 app = Flask(__name__)
 app_logger = create_logger(app)
@@ -16,8 +18,6 @@ TIMESTAMP_DIR = DATA_DIR.joinpath("timestamp")
 PROMETHEUS_URL = "http://prometheus:9090"
 
 step_seconds=10
-QUERY_RX_BPS = f'irate(container_network_receive_bytes_total{{name=~"clab-.*"}}[{step_seconds}s])*8'
-
 def get_timestamp_filepath(network: str, snapshot: str, action: str) -> Path:
     return TIMESTAMP_DIR.joinpath(f"{network}-{snapshot}-{action}.txt")
 
@@ -26,13 +26,13 @@ def save_timestamp(network: str, snapshot: str, action: str) -> None:
     if not path.parent.exists():
         path.parent.mkdir(exist_ok=True)
     with open(path, 'w') as f:
-        f.write(str(datetime.now(timezone.utc).timestamp()))
+        f.write(str(floor(datetime.now(timezone.utc).timestamp())))
 
-def get_timestamp(network: str, snapshot: str, action: str) -> str:
+def get_timestamp(network: str, snapshot: str, action: str) -> int:
     path = get_timestamp_filepath(network, snapshot, action)
     with open(path, 'r') as f:
         timestamp = f.read()
-    return timestamp
+    return int(floor(float(timestamp)))
 
 @app.route("/state-conductor/environment/<network>/<snapshot>/sampling", methods=["POST"])
 def post_sampling_action(network: str, snapshot: str):
@@ -62,9 +62,52 @@ def get_sampled_state_stats(network: str, snapshot: str):
 
     begin = get_timestamp(network, snapshot, 'begin')
     end   = get_timestamp(network, snapshot, 'end')
+    duration = end - begin
+
+    queries = {
+        "RX_BPS_AVG": f'avg_over_time(irate(container_network_receive_bytes_total{{instance="namespace-relabeler:5000",name=~"clab-.*"}}[15s])[{duration}s:])*8',
+        "RX_BPS_MAX": f'max_over_time(irate(container_network_receive_bytes_total{{instance="namespace-relabeler:5000",name=~"clab-.*"}}[15s])[{duration}s:])*8',
+        "RX_BPS_MIN": f'min_over_time(irate(container_network_receive_bytes_total{{instance="namespace-relabeler:5000",name=~"clab-.*"}}[15s])[{duration}s:])*8',
+        "TX_BPS_AVG": f'avg_over_time(irate(container_network_transmit_bytes_total{{instance="namespace-relabeler:5000",name=~"clab-.*"}}[15s])[{duration}s:])*8',
+        "TX_BPS_MAX": f'max_over_time(irate(container_network_transmit_bytes_total{{instance="namespace-relabeler:5000",name=~"clab-.*"}}[15s])[{duration}s:])*8',
+        "TX_BPS_MIN": f'min_over_time(irate(container_network_transmit_bytes_total{{instance="namespace-relabeler:5000",name=~"clab-.*"}}[15s])[{duration}s:])*8',
+    }
+
+    required_keys_map = {
+        "device": "container_label_clab_node_name",
+        "interface": "interface",
+    }
+
+    ignored_ifname_patterns = [
+        r"^erspan\d+",
+        r"^gre\d+",
+        r"^gretap\d+",
+        r"^ip6tnl\d+",
+        r"^lsi",
+        r"^sit\d+",
+        r"^tunl\d+",
+        r"^irb",
+        r"^eth0",
+    ]
 
     client = PrometheusClient(PROMETHEUS_URL)
-    metrics = client.query_metrics(QUERY_RX_BPS, begin, end, step=step_seconds*2)
+    metrics = defaultdict(lambda: defaultdict(dict))
+    for metric_type, query in queries.items():
+        raw_metrics = client.query_instant_metrics(query, end)
+        for raw_metric in raw_metrics:
+            interface = raw_metric["metric"].get(required_keys_map["interface"])
+            if not interface:
+                app_logger.debug(f"interface is not found. skipping")
+                continue
+            if any(re.match(pattern, interface) for pattern in ignored_ifname_patterns):
+                app_logger.debug(f"{interface} is not needed. skipping")
+                continue
+            device = raw_metric["metric"].get(required_keys_map["device"])
+            if not device:
+                app_logger.debug(f"device is not found. skipping")
+                continue
+            value = raw_metric["value"][1] # 1個目がタイムスタンプ、2個目が値
+            metrics[device][interface][metric_type] = value
 
     state_data = {
         "network": network,
